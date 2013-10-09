c68cc0ea785de170e1d23caa51cbe937422ba807
tabito ohtani <koko1000ban@gmail.com>
2013-10-09 17:19:08 +0900

    Fixed bug with virtual box guest feature not installed

--- centos-6.4/scripts/base.sh
+++ centos-6.4/scripts/base.sh
@@ -4,4 +4,4 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
 yum -y install gcc make gcc-c++ automake autoconf kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel httpd-devel apr-devel apr-util-devel
 yum -y install curl curl-devel bind-utils file git mailx man ntp openssh-clients patch rsync screen sysstat dstat htop traceroute vim-enhanced
-yum -y install perl wget dkms ruby ruby-devel rubygems kernel-devel kernel-headers
+yum -y install perl wget ruby ruby-devel rubygems kernel-devel kernel-headers
--- centos-6.4/scripts/virtualbox.sh
+++ centos-6.4/scripts/virtualbox.sh
@@ -1,10 +1,19 @@
-# Bail if we are not running inside VirtualBox.
-[ `facter virtual` == "virtualbox" ] || {
-  exit 0
-}
+# # Bail if we are not running inside VirtualBox.
+# [ `facter virtual` == "virtualbox" ] || {
+#   exit 0
+# }
 
+# mkdir -p /mnt/virtualbox
+# mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+# sh /mnt/virtualbox/VBoxLinuxAdditions.run
+# umount /mnt/virtualbox
+# rm -rf /home/vagrant/VBoxGuest*.iso
+
+# Installing the virtualbox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
 mkdir -p /mnt/virtualbox
-mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt/virtualbox
 sh /mnt/virtualbox/VBoxLinuxAdditions.run
 umount /mnt/virtualbox
-rm -rf /home/vagrant/VBoxGuest*.iso
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
